e71e9f41265530ecc383dc973b1e36ca51c591ae
sparksac <sparksac@rose-hulman.edu>
2018-04-17 21:42:23 -0400

    complete

--- src/problem3.py
+++ src/problem3.py
@@ -2,12 +2,12 @@
 Exam 2, problem 3.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  April 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Anthony Sparks.  April 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 ###############################################################################
-# TODO: 2.
+# Done: 2.
 #   In this problem, you will go through the methods of the  Person  class
 #   that is defined below, one by one, in the order that they appear.
 #   For each method:
@@ -40,35 +40,43 @@ def main():
 
 class Person(object):
     def __init__(self, age):
+        self.age = age
         """
         What comes in:  The Person's age.
         Side effects: Sets instance variables as needed by the other methods.
         """
-        # TODO: Implement and test this method.
+        # Done: Implement and test this method.
 
     def get_age(self):
         """ Returns this Person's age. """
-        # TODO: Implement and test this method.
+        return self.age
+        # Done: Implement and test this method.
 
     def celebrate_birthday(self):
         """
         Increments this Person's age.
         ALso prints 'Happy birthday!'.
         """
-        # TODO: Implement and test this method.
+        self.age += 1
+        print('Happy Birthday!')
+        # Done: Implement and test this method.
 
     def celebrate_birthdays(self, n):
         """
         What comes in: A non-negative integer n.
         Side effects:  Calls the  celebrate_birthday  method  n  times.
         """
-        # TODO: Implement and test this method.
+        for k in range(n):
+            self.age += 1
+        return self.age
+        # Done: Implement and test this method.
 
     def older_person(self):
         """
         Returns a new Person whose age is twice this Person's age.
         """
-        # TODO: Implement and test this method.
+        self.age = self.age * 2
+        # Done: Implement and test this method.
 
 
 # -----------------------------------------------------------------------------
