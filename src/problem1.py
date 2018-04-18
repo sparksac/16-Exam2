a4d301f3803996cebb3daaf9a73a0f34be35f674
sparksac <sparksac@rose-hulman.edu>
2018-04-17 21:27:26 -0400

    Don't know sequence format

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  April 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Anthony Sparks.  April 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -12,7 +12,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the code of the  Rect  class below.
+# Done: 2.  READ the code of the  Rect  class below.
 #
 #   Once you are confident that you understand the  Rect  class and its code,
 #   change the TO-DO for this problem to DONE.
@@ -29,12 +29,24 @@ class Rect(object):
 def run_test_problem1():
     """ Tests the   problem1   function. """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement at least 2 tests of the  problem1  function.
+    # Done: 3. Implement at least 2 tests of the  problem1  function.
     # -------------------------------------------------------------------------
     print()
     print('--------------------------------------------------')
     print('Testing the   problem1   function:')
     print('--------------------------------------------------')
+
+    # Test 1
+    expected = 150
+    actual = problem1(rectangles)
+    print(expected)
+    print(actual)
+
+    # Test 2
+    expected = 190
+    actual = problem1(rectangles)
+    print(expected)
+    print(actual)
 
 
 def problem1(rectangles):
@@ -52,6 +64,11 @@ def problem1(rectangles):
     :param rectangles: [Rect]
     :return: int
     """
+    x = rectangles
+    rectangles = [Rect(3, 10), Rect(5, 10), Rect(7, 10)]
+    sun = (x[0].w * x[0].h) + (x[1].w * x[1].h) + (x[2].w * x[2].h)
+    return sun
+
     # -------------------------------------------------------------------------
     # TODO: 4. Implement and test this function.
     # -------------------------------------------------------------------------
